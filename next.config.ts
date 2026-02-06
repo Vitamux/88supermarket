3fd334d519b520016bdffc4f9943ca648cc28dd1
Vitamux <harutyunyananri77@gmail.com>
2026-02-06 21:50:54 +0400

    88-supermarket demo

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
@@ -14,8 +15,14 @@ const nextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'qbfjppwrtjkcwfdrrqqr.supabase.co',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
